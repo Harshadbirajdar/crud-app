c4945046fc9a7e9e6c433eb22c649b26d2a46f7a
Harshad <hbirajdar1@gmail.com>
2023-02-25 19:40:29 +0530

    ID validation added

--- backend/src/validations/client.validation.ts
+++ backend/src/validations/client.validation.ts
@@ -6,6 +6,7 @@ const createClient = Joi.object().keys({
   email: Joi.string().email().message("Please enter valid email"),
   id: Joi.string()
     .required()
+    .max(13)
     .pattern(
       /(([0-9]{2})(0|1)([0-9])([0-3])([0-9]))([ ]?)(([0-9]{4})([ ]?)([0-1][8]([ ]?)[0-9]))/
     )
