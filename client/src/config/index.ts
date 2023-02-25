669cdca40ed3a21363ddb4bef6d5d7f6ff5376d8
Harshad <hbirajdar1@gmail.com>
2023-02-25 09:20:20 +0530

    Axios instance added

---
+++ client/src/config/index.ts
@@ -0,0 +1,2 @@
+export const API =
+  process.env.NEXT_PUBLIC_API || "https://localhost:4000/api/v1";
---
+++ client/src/helper/AxiosInstances.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { API } from "../config";
+import useStorage from "../hooks/useStorage";
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const { getItem } = useStorage();
+const token = getItem("token");
+
+const AxiosInstances = axios.create({
+  baseURL: API,
+  headers: {
+    Authorization: `${token ? `Bearer ${token}` : ""}`,
+  },
+});
+
+AxiosInstances.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.data.success == false) {
+      let message = error.response.data.message;
+      message ? toast.error(message) : toast.error("Something went wrong");
+    } else {
+      toast.error("Something went wrong");
+    }
+    throw error;
+  }
+);
+
+export default AxiosInstances;
